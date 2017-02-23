de07aba603c23d1cefaefe09321ee05863a3fd65
Zoltan Arvai <zoltan.arvai@live.com>
2017-02-23 18:34:50 +0700

    Added Redux and DevTools support for ios

--- index.ios.js
+++ index.ios.js
@@ -1,32 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, { Component } from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './src/reducers'
+import devToolsEnhancer from 'remote-redux-devtools'
 
-import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+  View,
+  StyleSheet
+} from 'react-native'
+
+const initialState = {}
+const store = createStore(
+  rootReducer,
+  initialState,
+  devToolsEnhancer()
+)
 
 export default class ContactsManager extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+        </View>
+      </Provider>
     );
   }
 }
@@ -37,17 +34,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('ContactsManager', () => ContactsManager);
